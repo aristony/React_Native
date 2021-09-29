0354076418d78c2d70960229cfa3be49110fb535
Ari Ramdhan <50137246+aristony@users.noreply.github.com>
2021-09-29 13:55:58 +0700

    Add files via upload

---
+++ CatatanPenting1/ReactNative/App1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text><Text style = {{fontWeight:'bold'}}>Selamat</Text> Datang Di <Text style = {{fontStyle:'italic'}}>Tutorial </Text>ini </Text>
+    </View>
+  )
+}
